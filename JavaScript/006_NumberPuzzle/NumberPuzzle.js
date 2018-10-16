0a8f802286873e3fa8544444297c5e91fee8b163
Knight Huang <knight720@outlook.com>
2018-10-17 00:47:33 +0800

    006 數字拼圖，鍵盤操作

--- JavaScript/006_NumberPuzzle/NumberPuzzle.js
+++ JavaScript/006_NumberPuzzle/NumberPuzzle.js
@@ -1,19 +1,55 @@
+var size = 3;
+
 $(function() {
-    console.log("ready!");
+    console.log(this);
+
     var log = document.getElementById('console');
 
     tableCreate();
 
     window.addEventListener('keydown', function(e) {
-        var code = e.keyCode ? e.keyCode : e.which;
-        this.console.log(code);
+        let valid = false;
+
+        let code = e.keyCode ? e.keyCode : e.which;
         log.innerHTML = code;
-        if (code == 78 && code == 17) { //ctrl + n
-            e.preventDefault();
-            alert("no new window");
-            //further actions
+
+        let elements = this.document.getElementsByClassName('current');
+        let current = elements[0];
+        let index = parseInt(current.id);
+        let currentValue = current.innerHTML;
+
+        log.innerHTML = '';
+        if (code == 37 && (index % size != 1)) { //left arrow
+            valid = true;
+            log.innerHTML = 'left arrow';
+            index = index - 1;
+        } else if (code == 39 && (index % size != 0)) { //right arrow
+            valid = true;
+            log.innerHTML = 'right arrow';
+            index = index + 1;
+        } else if (code == 38 && (index > size)) { //up arrow 
+            valid = true;
+            log.innerHTML = 'up arrow';
+            index = index - 3;
+        } else if (code == 40 && (index <= (size * (size - 1)))) { //down arrow
+            valid = true;
+            log.innerHTML = 'down arrow';
+            index = index + 3;
         }
-    })
+
+        if (valid) {
+            current.classList.remove("current");
+            let next = document.getElementById(index);
+            next.classList.add("current");
+            let nextValue = next.innerHTML;
+            current.innerHTML = nextValue;
+            next.innerHTML = currentValue;
+
+            isFinish();
+        }
+
+        //log.innerHTML = code + '-' + index;
+    });
 });
 
 function tableCreate() {
@@ -24,15 +60,16 @@ function tableCreate() {
     tbl.setAttribute('border', '1');
     var tbdy = document.createElement('tbody');
     let value = 0;
-    let size = 3;
     let last = size * size;
     for (var i = 0; i < size; i++) {
         var tr = document.createElement('tr');
         for (var j = 0; j < size; j++) {
             var td = document.createElement('td');
             td.appendChild(document.createTextNode(++value))
+            td.id = value;
+            td.classList.add('button');
             if (value === last) {
-                td.className = "current";
+                td.classList.add('current');
             }
             tr.appendChild(td)
         }
@@ -40,4 +77,17 @@ function tableCreate() {
     }
     tbl.appendChild(tbdy);
     body.appendChild(tbl)
+}
+
+function isFinish() {
+    let valid = true;
+    let buttons = document.getElementsByClassName('button');
+
+    buttons.forEach(i => {
+        console.log(i.id);
+        valid = i.innerHTML == i.id;
+        if (!valid) break;
+    });
+
+    if (valid) alert('Finish!');
 }
