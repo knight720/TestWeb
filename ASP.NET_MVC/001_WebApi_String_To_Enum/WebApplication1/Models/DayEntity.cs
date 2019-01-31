9a2c561a0be3b0fcd71460a3f7b65f8c8a485b57
Knight Huang <knighthuang@91app.com>
2019-01-31 15:26:37 +0800

    001 - 測試 POST 參數

---
+++ ASP.NET_MVC/001_WebApi_String_To_Enum/WebApplication1/Models/DayEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class DayEntity
+    {
+        public DayOfWeek DOW { get; set; }
+        public int Value { get; set; }
+        public string Text { get; set; }
+
+        public string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"DOM: {this.DOW.ToString()}");
+            sb.AppendLine($"Value: {this.Value.ToString()}");
+            sb.AppendLine($"Text: {this.Text}");
+            return sb.ToString();
+        }
+    }
+}
